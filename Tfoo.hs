80d576c95b97c3bdcfa81e6793192e2d7e85a869
Bartlomiej Niemtur <nbartlomiej@gmail.com>
2012-03-14 21:55:01 +0100

    Proof of concept, css and jquery in external files

--- Tfoo.hs
+++ Tfoo.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TypeFamilies, QuasiQuotes, MultiParamTypeClasses,
              TemplateHaskell, OverloadedStrings #-}
 import Yesod
+import Yesod.Static
 import Takefive
 import Control.Concurrent.Chan
 import Data.Text as T
@@ -8,10 +9,12 @@ import Data.List as L
 import Data.Maybe as M
 import System.Random as R
 import Control.Concurrent.MVar as V
+import Control.Monad as MD
 import Data.Monoid
-import Blaze.ByteString.Builder.Char.Utf8 (fromText)
+import Blaze.ByteString.Builder.Char.Utf8 (fromString)
 import Network.Wai.EventSource (ServerEvent (..), eventSourceApp)
 import Text.Hamlet (hamletFile)
+import Text.Lucius (luciusFile)
 
 type Player = String
 data Game = Game {
@@ -39,7 +42,8 @@ nextMark board = if (count X) <= (count O) then X else O where
 data Tfoo = Tfoo {
     seed       :: Int,
     games      :: MVar [IO Game],
-    nextGameId :: MVar Int
+    nextGameId :: MVar Int,
+    tfooStatic :: Static
   }
 
 mkYesod "Tfoo" [parseRoutes|
@@ -50,6 +54,7 @@ mkYesod "Tfoo" [parseRoutes|
 /games/#Int/join/x          PlayerXR POST
 /games/#Int/mark/#Int/#Int  MarkR POST
 /games/#Int/listen          ChannelR GET
+/static                     StaticR Static tfooStatic
 |]
 
 instance Yesod Tfoo
@@ -57,32 +62,34 @@ instance Yesod Tfoo
 getHomeR :: Handler RepHtml
 getHomeR = do
   tfoo <- getYesod
-  defaultLayout [whamlet|
-    <p>
-      Start a new Take Five game with a human opponent
-    <form method=post action=@{GamesR}>
-      <input type=submit value="NEW GAME">
-  |]
+  defaultLayout $ do
+    addStylesheet $ StaticR $ StaticRoute ["styles", "tfoo.css"] []
+    [whamlet|
+      <div .landing-page>
+        <h1> TFOO!
+        <h2> Take Five Online, Obviously.
+        <div>
+          An implementation of Take Five game (wikipedia) that utilizes Haskell,
+          Yesod (Haskell web framework) and EventSource (http://caniuse.com/eventsource).
+        <div>
+          Start a new Take Five game with a human opponent
+          <form method=post action=@{GamesR}>
+            <input type=submit value="NEW GAME">
+        <div> Read a blog post:
+        <div> View source on GitHub:
+    |]
 
 postGamesR :: Handler RepHtml
 postGamesR = do
     tfoo <- getYesod
     id   <- liftIO $ newGameId tfoo
     redirect $ GameR id
-  where newGameId tfoo = modifyMVar (nextGameId tfoo) incrementMVar
-        incrementMVar value = return (value+1, value)
+  where -- Increment tfoo's 
+        newGameId :: Tfoo -> IO Int
+        newGameId tfoo = modifyMVar (nextGameId tfoo) incrementMVar
 
-joinGame :: Int -> Mark -> Handler ()
-joinGame id mark =
-  do
-    game <- getGame id
-    tfoo <- getYesod
-    -- setSession "player" $ T.pack (playerId tfoo)
-    appendSession' "players" $ T.pack (playerId tfoo)
-    updateGame id $ setPlayer game mark (playerId tfoo)
-    return ()
-  where
-    playerId tfoo = (show $ seed tfoo) ++ (show id) ++ (show mark)
+        incrementMVar :: Int -> IO (Int, Int)
+        incrementMVar value = return (value+1, value)
 
 getGameR :: Int -> Handler RepHtml
 getGameR id = let
@@ -93,14 +100,6 @@ getGameR id = let
     maybePlayers <- lookupSession "players"
     tfoo <- getYesod
     defaultLayout $ do
-      toWidgetHead [lucius|
-        #channel {
-          background: #ccc;
-          width: 400px;
-          height: 200px;
-        }
-      |]
-      addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"
       toWidgetHead [julius|
         var post = function(url){
           var xhr = new XMLHttpRequest();
@@ -110,16 +109,22 @@ getGameR id = let
         $(document).ready(function() {
           var src = new EventSource("@{ChannelR id}");
           src.onmessage = function(input) {
-            $("#channel").append(document.createTextNode(input.data));
             console.log(input.data);
             var message = JSON.parse(input.data);
-            if (message.id == "player-joined"){
+            if (message.id == "player-new"){
               $("#no_player_"+message.side).replaceWith("<div>Joined</div>");
-            } else if (message.id == "post-mark") {
-              console.log(message);
+            } else if (message.id == "mark-new") {
+              var markId = "#cell_"+message.x+"_"+message.y;
+              $(markId).replaceWith(
+                "<div id='"+markId+"' class='mark-"+message.mark+"'></div>"
+              );
+            } else if (message.id == "alert") {
+              $("#messages").prepend(
+                "<div class='message'>"+message.content+"</div>"
+              );
             }
           };
-          $('.post-mark').each(function(index, element){
+          $('.mark-new').each(function(index, element){
             $(element).click(function(){
               var x = $(element).attr('data-x');
               var y = $(element).attr('data-y');
@@ -128,9 +133,13 @@ getGameR id = let
           });
         });
       |]
+      addStylesheet $ StaticR $ StaticRoute ["styles", "tfoo.css"] []
+      addScript $ StaticR $ StaticRoute ["scripts","jquery-1.7.1.min.js"] []
       [whamlet|
         <div .players>
           <div #player_x>
+            <div .player-description>
+              Cross:
             $maybe player <- (playerX game)
               <div #joined >
                 Joined
@@ -139,10 +148,12 @@ getGameR id = let
                     (You)
                   $else
             $nothing
-              <div #no_player_X >
+              <div .player-join #no_player_X >
                 <form method=post action=@{PlayerXR id}>
                   <input value="Join as X" type=submit>
           <div #player_o>
+            <div .player-description>
+              Circle:
             $maybe player <- (playerO game)
               <div #joined >
                 Joined
@@ -151,37 +162,62 @@ getGameR id = let
                     (You)
                   $else
             $nothing
-              <div #no_player_O >
+              <div .player-join #no_player_O >
                 <form method=post action=@{PlayerOR id}>
                   <input value="Join as O" type=submit>
+        <div #messages>
         <table #board>
           $forall column <- columns
             <tr>
               $forall row <- rows
                 <td>
                   $maybe mark <- getCell (board game) row column
-                    mark
+                    <div #cell_#{row}_#{column} .mark-#{show mark} data-x=#{row} data-y=#{column}>
                   $nothing
-                    <div #cell_#{row}_#{column} ."post-mark" data-x=#{row} data-y=#{column}>
-                      empty
-        <div #channel>
+                    <div #cell_#{row}_#{column} ."mark-new" data-x=#{row} data-y=#{column}>
       |]
 
 postMarkR :: Int -> Int -> Int -> Handler ()
 postMarkR id x y = do
-  game <- getGame id
-  maybePlayers <- lookupSession "players"
-  require $ maybePlayers /= Nothing
-  require $ (whoseTurn game) /= Nothing
-  require $ (getCell (board game) x y) == Nothing
-  require $ elem (fromJust $ whoseTurn $ game) (L.words $ T.unpack $ fromJust maybePlayers)
-  updateGame id $ game { board = replace' x y (Just $ nextMark (board game)) (board game)}
-  broadcast id "post-mark" [("x", show x),("y", show y),("mark",show (nextMark $ board game))]
+    game               <- getGame id
+    whoseTurn'         <- return $ whoseTurn game
+    board'             <- return $ board game
+    userAuthorizations <- do
+      authorizations <- lookupSession "players"
+      return $ fmap (L.words . T.unpack) authorizations
 
-require :: Bool -> Handler ()
-require result = if result == False
-  then permissionDenied "Permission Denied"
-  else return ()
+    -- The target cell has to be empty.
+    require $ (getCell (board game) x y) == Nothing
+    -- User has to be authorized to make this move
+    require $ fromMaybe False (MD.liftM2 elem whoseTurn' userAuthorizations)
+    -- The game has to be still in progress
+    require $ (winner board') == Nothing
+
+    updateGame id $ game {board = replace' x y (Just $ nextMark board') board'}
+
+    broadcast id "mark-new" [
+        ("x", show x), ("y", show y), ("mark", show (nextMark board'))
+      ]
+
+    broadcastGameState id
+
+  where require result = if result == False
+          then permissionDenied "Permission Denied"
+          else return ()
+        elem' x y = (elem . L.words . T.unpack)
+        userAuthorizations' = L.words . T.unpack
+
+broadcastGameState :: Int -> Handler ()
+broadcastGameState id = do
+    game  <- getGame id
+    board' <- return $ board game
+    maybe (notifyNextPlayer board') announceWinner (winner board')
+  where
+    notifyNextPlayer board =
+      broadcast id "alert" [("content", (show $ nextMark board)++"'s turn")]
+    announceWinner mark =
+      broadcast id "alert" [("winner", "Game won: "++(show mark))]
+
 
 postPlayerOR :: Int -> Handler RepHtml
 postPlayerOR id = do
@@ -189,7 +225,7 @@ postPlayerOR id = do
   if (playerO game) == Nothing
     then do
       joinGame id O
-      broadcast id "player-joined" [("side", "O")]
+      broadcast id "player-new" [("side", "O")]
       return ()
     else return ()
   redirect $ GameR id
@@ -201,7 +237,7 @@ postPlayerXR id = do
   if (playerX game) == Nothing
     then do
       joinGame id X
-      broadcast id "player-joined" [("side", "X")]
+      broadcast id "player-new" [("side", "X")]
       return ()
     else return ()
   redirect $ GameR id
@@ -218,11 +254,22 @@ getChannelR id = do
 broadcast :: Int -> String -> [(String, String)] -> Handler ()
 broadcast gameId messageId pairs = do
   game <- getGame gameId
-  liftIO $ writeChan (channel game) $ serverEvent $ return $ fromText message
-  where message = T.pack $ "{"++(stringifiedPairs $ ("id",messageId):pairs)++"}"
+  liftIO $ writeChan (channel game) $ serverEvent $ return $ fromString message
+  where message = "{"++(stringifiedPairs $ ("id",messageId):pairs)++"}"
         stringifiedPairs pairs = L.intercalate ", " $ L.map stringifyPair pairs
         stringifyPair p = "\""++(fst p) ++ "\": \"" ++ (snd p) ++ "\""
         serverEvent = ServerEvent Nothing Nothing
+
+joinGame :: Int -> Mark -> Handler ()
+joinGame id mark =
+  do
+    game <- getGame id
+    tfoo <- getYesod
+    appendSession' "players" $ T.pack (playerId tfoo)
+    updateGame id $ setPlayer game mark (playerId tfoo)
+    return ()
+  where
+    playerId tfoo = (show $ seed tfoo) ++ (show id) ++ (show mark)
 
 -- Appends the given value to the session key.
 appendSession :: Text -> Text -> Handler ()
@@ -252,7 +299,6 @@ updateGame id game = do
     )
   return ()
 
-
 createGame :: IO Game
 createGame = do
   channel <- newChan
@@ -271,4 +317,5 @@ main = do
   nextGameId <- newMVar 1
   games <- newMVar gameStream
   seedP <- liftIO $ getStdGen >>= (\x -> return $ next x)
-  warpDebug 3000 (Tfoo (fst seedP) games nextGameId)
+  static' <- static "static"
+  warpDebug 3100 (Tfoo (fst seedP) games nextGameId static')
